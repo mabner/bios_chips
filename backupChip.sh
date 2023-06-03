9f911b5b558a9c0eb494fd9014481d80144f5cd9
Marcos Leite <m.abner@live.co.uk>
2023-06-02 23:42:34 -0300

    backup: asks for filename and adds date and time to it

--- backupChip.sh
+++ backupChip.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-flashrom -p linux_spi:dev=/dev/spidev0.0,spispeed=512 -r backup_chip
+filedate=$(date +"%d.%m.%Y_%H.%M.%S")
+echo "Inform a name for the ROM backup: "
+read name
+filename=$name"_"$filedate
+flashrom -p linux_spi:dev=/dev/spidev0.0,spispeed=512 -r $filename
+ls $filename
+
