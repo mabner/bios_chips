648cf1d7e81a3be1cbf4bb4b9a1147d3d2a91547
Marcos Leite <m.abner@live.co.uk>
2023-06-02 23:44:34 -0300

    write: asks for the filename

--- writeChip.sh
+++ writeChip.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-flashrom -p linux_spi:dev=/dev/spidev0.0,spispeed=512 -w IPM31REV103.BIN
+echo "Inform the name of the ROM file to write to chip: "
+read filename
+flashrom -p linux_spi:dev=/dev/spidev0.0,spispeed=512 -w $filename
